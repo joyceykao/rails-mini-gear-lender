95d6458d2dc7784e4b0ee3938a1d95bf0eee27da
Joyce Kao <joyceykao@gmail.com>
2020-04-23 11:40:02 +0200

    redirected path to newly created item instead of index page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,9 +31,7 @@ class ItemsController < ApplicationController
     @item = Item.new(item_params)
     @item.user = current_user
     if @item.save
-      # uncomment below when show method/view is complete
-      # redirect_to item_path(@item)
-      redirect_to items_path(index)
+      redirect_to item_path(@item)
     else
       render :new
     end
